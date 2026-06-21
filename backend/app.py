4d2c572ef4072a7142c2e3935be3f77c6e413c5d
Ashu5622 <ashutoshshrivastav40@gmail.com>
2026-06-21 16:51:09 +0530

    Setup database and user model

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 
+from config import Config
+from extensions import db
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    from models.user import User
+    db.create_all()
 
 @app.route("/")
 def home():
